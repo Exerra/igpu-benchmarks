b9fcc69a0d1ddac184b6d981bc0df55f0f1de312
Exerra <exerra@exerra.xyz>
2022-10-04 20:00:01 +0300

    test

--- app/util/siteinfo.ts
+++ app/util/siteinfo.ts
@@ -1,5 +1,5 @@
 export default {
-	title: "Bench",
+	title: "Benchmarks",
 	image: "https://cdn.exerra.xyz/png/pfp.png",
 	keywords: "benchmarks, exerra, gpu, igpu, integrated gpu, game",
 	description: "GPU benchmarks for games ranging from indie platformers to AAA shooters"
